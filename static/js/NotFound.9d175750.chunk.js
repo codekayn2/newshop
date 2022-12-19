d09342aa17a22b64e1e353efb59d279c48387fda
codekayn <vadim.poznakhivski@gmail.com>
2022-12-20 00:16:54 +0200

    Updates

---
+++ static/js/NotFound.9d175750.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshop_pizza=self.webpackChunkshop_pizza||[]).push([[187],{4949:function(s,u,e){e.r(u);e(2791);var n=e(3245),p=e(184);u.default=function(){return(0,p.jsx)(n.vr,{})}}}]);
+//# sourceMappingURL=NotFound.9d175750.chunk.js.map
